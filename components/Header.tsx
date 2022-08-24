31751dfe77fb434ffa46e2e715594ec3d1d622e4
Ricardo Ocampo <rocampocastro604@gmail.com>
2022-08-24 17:00:16 -0500

    Added Header component

---
+++ components/Header.tsx
@@ -0,0 +1,51 @@
+import {BellIcon, SearchIcon} from '@heroicons/react/solid'
+import Link from 'next/link'
+import { useEffect, useState } from 'react'
+
+function Header() {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      window.scrollY > 0 ? setIsScrolled(true) : setIsScrolled(false);
+      }
+      window.addEventListener('scroll', handleScroll)
+      return () => {
+        window.removeEventListener('scroll', handleScroll)
+      }
+  }, [])
+  return (
+    <header className={`${isScrolled && "bg-[#141414]"}`}>
+      <div className="flex items-center space-x-2 md:space-x-10">
+      <img
+          src="https://bemaster.com/wp-content/uploads/2022/03/Logo-05.png"
+          width={200}
+          height={200}
+          className="cursor-pointer object-contain"
+        />
+        <ul className="hidden space-x-4 md:flex">
+          <li className="headerLink">Home</li>
+          <li className="headerLink">TV Shows</li>
+          <li className="headerLink">Movies</li>
+          <li className="headerLink">New and Popular</li>
+          <li className="headerLink">My List</li>
+        </ul>
+      </div>
+
+      <div className='flex items-center space-x-4 text-sm font-light'>
+        <SearchIcon className="hidden sm:inline h-6 w-6"/>
+        <p className='hiddn lg:inline'>Kids</p>
+        <BellIcon className='h-6 w-6'/>
+        <Link href='/account'>
+        <img
+            src="https://rb.gy/g1pwyx"
+            alt=""
+            className="cursor-pointer rounded"
+          />
+        </Link>
+      </div>
+    </header>
+  )
+}
+
+export default Header
